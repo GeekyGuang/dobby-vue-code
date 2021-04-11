57eac903b0e0564305ab709eb9684f41f40008bc
TravisWongX <travis.x.wong@gmail.com>
2021-04-11 20:22:33 +0800

    创建3个页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    hello
+    <router-view/>
+    <router-link to="/money">记账</router-link> |
+    <router-link to="/labels">标签</router-link> |
+    <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
