ae9924ce916f63ab0a8a6b11befefcfecf32e9e8
TravisWongX <travis.x.wong@gmail.com>
2021-04-19 16:59:46 +0800

    安装和配置svg-sprite-loader

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,14 @@
 <template>
   <Layout>
-    money.vue
+    <svg>
+      <use xlink:href="#money" />
+    </svg>
   </Layout>
 </template>
 
 <script lang="ts">
+ import x from '@/assets/icons/money.svg'
+ console.log(x)
 export default {
 name: "Money",
 }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const path = require('path')
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)  // 以.svg结尾
+      .include.add(dir).end()  // 包含icons目录
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()  // 配置svg-sprite-loader
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])  // 配置插件
+    config.module.rule('svg').exclude.add(dir)  // 其他svg loader排除icons目录
+  }
 }
