7861fffc6a07129aef4d365c31c1b0966009c45c
travis <travis.x.wong@gmail.com>
2021-04-19 20:09:36 +0800

    封装icon

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,14 +1,10 @@
 <template>
   <Layout>
-    <svg>
-      <use xlink:href="#money" />
-    </svg>
   </Layout>
 </template>
 
 <script lang="ts">
- import x from '@/assets/icons/money.svg'
- console.log(x)
+
 export default {
 name: "Money",
 }
