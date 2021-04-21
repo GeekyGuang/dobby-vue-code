bc2604058b74506c417c57dc7b9e335707ec869e
TravisWongX <travis.x.wong@gmail.com>
2021-04-21 11:11:29 +0800

    svgo-loader去除fill

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,8 @@ module.exports = {
       .test(/\.svg$/)  // 以.svg结尾
       .include.add(dir).end()  // 包含icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()  // 配置svg-sprite-loader
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options=>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end();
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])  // 配置插件
     config.module.rule('svg').exclude.add(dir)  // 其他svg loader排除icons目录
   }
