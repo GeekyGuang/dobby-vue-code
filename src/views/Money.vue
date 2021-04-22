f5a666c2ffb758df8aacadeae8e7fb4007441c96
TravisWongX <travis.x.wong@gmail.com>
2021-04-22 17:44:11 +0800

    money html

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,25 @@
 <template>
   <Layout>
+    <div class="tags">
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+     <div class="new">
+       <button>新增标签</button>
+     </div>
+    </div>
+    <div class="notes">
+      <label>
+        <span class="name">备注</span>
+        <input type="text">
+      </label>
+    </div>
+    <div>type</div>
+    <div>number pad</div>
+
   </Layout>
 </template>
 
