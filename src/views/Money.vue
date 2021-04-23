4a4d58a29438c689c5f80a43ddb4aa49bebd9af5
TravisWongX <travis.x.wong@gmail.com>
2021-04-24 07:23:40 +0800

    添加全局样式和变量

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,9 +17,31 @@
         <input type="text">
       </label>
     </div>
-    <div>type</div>
-    <div>number pad</div>
-
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button></button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>OK</button>
+        <button>.</button>
+      </div>
+    </div>
   </Layout>
 </template>
 
