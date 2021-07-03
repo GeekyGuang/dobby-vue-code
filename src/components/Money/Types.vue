bc34a5a40e4164ff7222848f17f006720c6bef75
travis <travis.x.wong@gmail.com>
2021-07-03 16:45:12 +0800

    type use .sync

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type === '-' && 'selected'"
+      <li :class="value === '-' && 'selected'"
           @click="selectType('-')">支出
       </li>
-      <li :class="type === '+' && 'selected'"
+      <li :class="value === '+' && 'selected'"
           @click="selectType('+')">收入
       </li>
     </ul>
@@ -17,16 +17,13 @@ import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-  type = '-';  // - 表示支出 + 表示收入
+  @Prop() readonly value!:string
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
       throw new Error('type is unknown');
+    }else{
+      this.$emit('update:value', type)
     }
-    this.type = type;
-  }
-  @Watch('type')
-  onTypeChanged(value:string){
-    this.$emit('update:value', value)
   }
 
 }
