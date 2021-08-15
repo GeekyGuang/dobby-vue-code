331d6dc7813fbbb08c9488864bfcb6dca7ccf889
travis <travis.x.wong@gmail.com>
2021-08-15 15:46:56 +0800

    使用mixin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,6 +18,8 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import {mixins} from 'vue-class-component';
+import {tagHelper} from '@/mixins/tagHelper';
 
 @Component({
   components: {Button},
@@ -27,16 +29,9 @@ import Button from '@/components/Button.vue';
     }
   }
 })
-export default class Labels extends Vue {
+export default class Labels extends mixins(tagHelper) {
   created() {
     this.$store.commit('fetchTags')
-  }
-  createTag() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {
-      return window.alert('标签名不可为空')
-    }
-    this.$store.commit('createTag', name)
   }
 }
 </script>
