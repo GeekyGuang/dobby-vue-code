81e47b83b2703bb5af4a6f3bcd27a7a73e5db75d
travis <travis.x.wong@gmail.com>
2021-08-15 20:23:07 +0800

    refactor tag to currentTag

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name"
+      <FormItem :value="currentTag.name"
                 @update:value="update"
                 field-name="标签名"
                 placeholder="请输入标签名"/>
@@ -26,27 +26,27 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem},
 })
 export default class EditLabel extends Vue{
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag
   }
 
   created(){
     this.$store.commit('fetchTags')
     this.$store.commit('setCurrentTag',this.$route.params.id)
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404')
     }
   }
 
   update(name:string):void {
-    if(this.tag){
-      this.$store.commit('updateTag',{id:this.tag.id, name})
+    if(this.currentTag){
+      this.$store.commit('updateTag',{id:this.currentTag.id, name})
     }
   }
 
   remove():void {
-    if(this.tag) {
-      this.$store.commit('removeTag',this.tag.id)
+    if(this.currentTag) {
+      this.$store.commit('removeTag',this.currentTag.id)
     }
   }
 
