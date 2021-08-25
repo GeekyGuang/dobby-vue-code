f74996b1d1dc7263f0ebfe33d7f6b63df804c0a7
travis <travis.x.wong@gmail.com>
2021-08-25 23:30:50 +0800

    完成展示数据的数据逻辑和html部分

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,16 @@
   <Layout>
     <Tabs class-prefix="type" :value.sync="type" :data-source="recordTypeList"/>
     <Tabs class-prefix="interval" :value.sync="interval" :dataSource="intervalList"/>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3>{{group.title}}</h3>
+        <ol>
+          <li v-for="(item,index) in group.items" :key="index">
+            {{item.amount}} {{item.createAt}}
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -21,6 +31,26 @@ export default class Statistics extends  Vue{
 
     intervalList = intervalList
     recordTypeList = recordTypeList
+
+    get recordList() {
+      return this.$store.state.recordList
+    }
+
+    get result(){
+      const {recordList} = this;
+      type HashTableValue = {title: string, items: RecordItem[]}
+      const hashTable: {[key: string]: HashTableValue} = {}
+      for(let i = 0; i < recordList.length; i++){
+         const [date,time] = recordList[i].createAt.split('T')
+         hashTable[date] = hashTable[date] || {title: date, items: []}
+         hashTable[date].items.push(recordList[i])
+      }
+      return hashTable
+    }
+
+    beforeCreate(){
+      this.$store.commit('fetchRecords')
+    }
 }
 </script>
 
