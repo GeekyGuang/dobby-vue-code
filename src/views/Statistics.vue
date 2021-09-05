3b1e81ee541c5708c13774151008b41e0ede6f39
travis <travis.x.wong@gmail.com>
2021-09-05 22:44:30 +0800

    深拷贝和泛型

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -24,6 +24,7 @@ import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs'
+import clone from '@/lib/clone';
 
 @Component({
   components:{Tabs}
@@ -63,13 +64,8 @@ export default class Statistics extends  Vue{
     get result(){
       const {recordList} = this;
       type HashTableValue = {title: string, items: RecordItem[]}
-      const hashTable: {[key: string]: HashTableValue} = {}
-      for(let i = 0; i < recordList.length; i++){
-         const [date,time] = recordList[i].createAt.split('T')
-         hashTable[date] = hashTable[date] || {title: date, items: []}
-         hashTable[date].items.push(recordList[i])
-      }
-      return hashTable
+      const newList = clone(recordList).sort((a,b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+      return []
     }
 
     beforeCreate(){
