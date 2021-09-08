97d740006547c00785f91c9f9065f12c9b1da412
travis <travis.x.wong@gmail.com>
2021-09-09 07:32:19 +0800

    部署项目到GitHub

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/morney-web/'
+    : '/',
   lintOnSave: false,
 
   chainWebpack: config => {
@@ -10,7 +13,7 @@ module.exports = {
       .include.add(dir).end()  // 包含icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()  // 配置svg-sprite-loader
       .use('svgo-loader').loader('svgo-loader')
-      .tap(options=>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end();
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])  // 配置插件
     config.module.rule('svg').exclude.add(dir)  // 其他svg loader排除icons目录
   }
