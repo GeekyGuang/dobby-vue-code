b6eb3f722d4c3ad619f53226d886a2c77209d7da
travis <travis.x.wong@gmail.com>
2021-09-09 12:22:59 +0800

    修复二维码路径问题

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,15 @@ new Vue({
 window.onload = () => {
   setTimeout(() => window.scrollTo(0,200), 0)
 }
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请在手机端打开本页面，以保证浏览效果')
+  const img = document.createElement('img')
+  img.src = './qrcode.png'
+  img.style.position = 'fixed'
+  img.style.left = '50%'
+  img.style.top = '50%'
+  img.style.transform = 'translate(-50%, -50%)'
+  img.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.25)'
+  document.body.appendChild(img)
+}
