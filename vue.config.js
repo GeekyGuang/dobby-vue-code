0bc010e0cb06daef6d769709b345d25ec65e1165
TravisWongX <travis.x.wong@gmail.com>
2022-04-06 17:25:57 +0800

    修改打包配置，绝对路径改为相对路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/dobby-vue/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   lintOnSave: false,
 
   chainWebpack: (config) => {
